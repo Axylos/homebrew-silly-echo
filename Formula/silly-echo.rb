eec785b65128fd4c9e5e0402307836722edd0113
axylos <robertdraketalley@gmail.com>
2022-05-22 00:28:15 -0400

    bump to rc6

--- Formula/silly-echo.rb
+++ Formula/silly-echo.rb
@@ -1,7 +1,7 @@
 class SillyEcho < Formula
   desc "Like echo but Silly"
   homepage "https://draketalley.com"
-  url "https://github.com/Axylos/silly/archive/refs/tags/v0.1.1-rc.2.tar.gz"
+  url "https://github.com/Axylos/silly/archive/refs/tags/v0.1.1-rc.6.tar.gz"
   version "0.1.1-rc.6"
   sha256 "3801a7aee6e22716b37f95470defb2e1bfd85abc69caab21be2abd5cf86df0e9"
   license "MIT"
